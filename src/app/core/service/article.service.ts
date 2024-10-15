42651026adeaf01fd5a466c821ba113ce5ea315e
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-10-14 22:10:16 -0500

    add save article method in article service

---
+++ src/app/core/service/article.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { ArticleRequest } from '@model/article-request.model';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+  private readonly CategoryUrl = environment.stokApi;
+  private readonly token = environment.token;
+  private errorMessage: string = '';
+
+  constructor(private readonly http:HttpClient) { }
+
+  saveArticle(article: ArticleRequest): Observable<void> {
+  const headers = new HttpHeaders({
+    Authorization: `Bearer ${this.token}`,
+    'Content-Type': 'application/json',
+  });
+  return this.http.post<void>(`${this.CategoryUrl}/article/save`, article, { headers }).pipe(catchError(this.getError.bind(this)));
+  }
+
+  private getError(error: HttpErrorResponse) {
+    const status = error.status;
+    if (status === 401 || status === 403) {
+      this.errorMessage = 'You are not authorized to perform this action';
+    }
+    if(status === 409) {
+      this.errorMessage = 'article already exists';
+    }
+    return throwError(() => error);
+  }
+
+  getErrorMessage(): string {
+    return this.errorMessage;
+  }
+}
