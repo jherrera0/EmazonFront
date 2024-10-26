c4bea4e9b61b549854b65de2948c8722b98f34be
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-10-26 18:13:16 -0500

    create auth service

---
+++ src/app/core/service/auth.service.ts
@@ -0,0 +1,32 @@
+import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { UserRequest } from '@model/user-request.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly authUrl = environment.authApi;
+  private readonly token = environment.token
+  private errorMessage: string = '';
+  constructor(private readonly http:HttpClient) { }
+
+  createAuxWarehouse(warehouse: UserRequest): Observable<void> {
+    const headers = {
+      Authorization: `Bearer ${this.token}`,
+      'Content-Type': 'application/json',
+    };
+    return this.http.post<void>(`${this.authUrl}/user/save`, warehouse, { headers }).pipe(catchError(this.getError.bind(this)));
+  }
+
+  private getError(error: HttpErrorResponse) {
+    this.errorMessage = error.error.message;
+    return throwError(() => error);
+  }
+
+  getErrorMessage(): string {
+    return this.errorMessage;
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   stokApi: 'http://localhost:8082',
+  authApi: 'http://localhost:8080',
   apiKey: 'DEV_API_KEY',
   token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQURNSU4iLCJJZCI6MSwic3ViIjoiam9zZUBnbWFpbC5jb20iLCJpYXQiOjE3Mjk5ODE1NTQsImV4cCI6MTcyOTk4MzM1NH0.vZoFJEuzZIILIve7u1ej0lqlPc0zATAY_6P2YJMgajk"
 };
