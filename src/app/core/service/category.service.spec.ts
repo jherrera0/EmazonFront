9279070ae7621cc63d0360c4e54a65d5e34fd9c2
Jose Herrera <jherreraa2@unicartagena.edu.co>
2024-10-26 17:41:55 -0500

    remove unused imports

--- src/app/core/service/category.service.spec.ts
+++ src/app/core/service/category.service.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CategoryService } from './category.service';
 import { CategoryRequest } from 'src/app/core/model/category-request.model';
-import { HttpErrorResponse } from '@angular/common/http';
 import { environment } from '@environments/environment';
 
 describe('CategoryService', () => {
